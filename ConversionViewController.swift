29db00bcabb4b73f48f47a9477c561d40eb9ad9c
Vlad Blajovan <vlad.blajovan@yahoo.com>
2016-03-23 17:06:44 +0200

    WorldTrotter v1.0

---
+++ ConversionViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Vlad Blajovan on 23.03.2016.
+//  Copyright © 2016 Vlad Blajovan. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view!")
+    }
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            //celsiusLabel.text = "\(value)"
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+}
---
+++ MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Vlad Blajovan on 23.03.2016.
+//  Copyright © 2016 Vlad Blajovan. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view!")
+    }
+}
